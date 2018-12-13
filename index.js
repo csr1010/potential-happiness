6690cc2bf39ece8c14a6607322fab81d3dddcf6d
Vanessa Wu <vanessa.wu@oracle.com>
2018-12-13 12:16:32 -0700

    deleted users.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import http from 'http';
 import indexRouter from './routes/index';
-import usersRouter from './routes/users';
 
 const server = http.Server(app);
 const app = express();
@@ -12,6 +11,5 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 server.listen(port, host, () => console.log(`Server running at http://${host}:${port}/`))
