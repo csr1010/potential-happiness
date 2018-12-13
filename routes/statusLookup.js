a67b8771a71355dbc91d03ec192c8d324447d969
Kevin Bergman <kevin.bergman@moat.com>
2018-12-13 15:22:04 -0700

    status Lookup

---
+++ routes/statusLookup.js
@@ -0,0 +1,15 @@
+import {Router} from "express"
+import dummyData from "../dummyData/jiras.json"
+
+const router = Router();
+
+export default () => {
+
+  router.get('/:ticketStatus', (req, res, next) => {
+    const ticketStatus = req.params.ticketStatus;
+    var result = dummyData.filter(ticket => ticket.status == ticketStatus);
+    res.json(result);
+  });
+
+  return router;
+}
