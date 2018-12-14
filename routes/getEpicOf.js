745fda176e7dc0ac583988f2322d710a57580768
Kevin Bergman <kevin.bergman@moat.com>
2018-12-13 17:32:40 -0700

    more case fixes

--- routes/getEpicOf.js
+++ routes/getEpicOf.js
@@ -7,7 +7,7 @@ export default () => {
 
   router.get('/:ticketId', (req, res, next) => {
     const ticketId = req.params.ticketId;
-    var ticket = dummyData.filter(ticket => ticket.Id == ticketId);
+    var ticket = dummyData.filter(ticket => ticket.IdId.toLowerCase() == ticketId.toLowerCase());
     var epic = ticket[0].epic;
     res.json(epic);
   });
