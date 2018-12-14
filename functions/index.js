7ab5c3dbc2a183c4f16fd45ef9c6f5c905d26c6b
Vanessa Wu <vanessa.wu@oracle.com>
2018-12-13 17:27:10 -0700

    fetch data from url

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 'use strict';
 const functions = require('firebase-functions');
+const axios= require('axios');
+const url = 'http://127.0.0.1:8082/api/'
 
 exports.fetchDialogFlowData = functions.https.onRequest((request, response) => {
   const results = {}
@@ -8,5 +10,9 @@ exports.fetchDialogFlowData = functions.https.onRequest((request, response) => {
   results.jira_type = parameters['jira_type'] || '';
   results.name = parameters['given-name'] || '';  
   process.env.DEBUG = 'dialogflow:debug';
+
+  axios.get(`${url}${results.jira_type}/${results.ticket_number}`)
+       .then((res) => console.log(res.data))
+       .catch((err) => console.log(err))
   response.send(JSON.stringify({"fulfillmentText": JSON.stringify(results)}));
 });
