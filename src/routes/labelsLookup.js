ad80001e47786d869aaca43d0652cff631c40598
Chetan Jannu <chetan.jannu@oracle.com>
2018-12-17 15:47:28 -0500

    babel changes

--- src/routes/labelsLookup.js
+++ src/routes/labelsLookup.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import dummyData from "../dummyData/jiras.json"
+import dummyData from "../../dummyData/index.json"
 
 const router = Router();
 
@@ -8,8 +8,8 @@ export default () => {
   router.get('/:ticketId', (req, res, next) => {
     const ticketId = req.params.ticketId;
     var ticket = dummyData.filter(ticket => ticket.Id.toLowerCase() == ticketId.toLowerCase());
-    var component = ticket[0].components;
-    res.json(component);
+    var labels = ticket[0].labels;
+    res.json(labels);
   });
 
   return router;
